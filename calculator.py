346f415a6560c7744a1306b07d062ffffb9b1f43
amirmazhar126 <amirmazhar126@gmail.com>
2022-07-23 00:21:31 +0530

    Added subtraction feature

--- calculator.py
+++ calculator.py
@@ -1,4 +1,4 @@
-from operations import add
+from operations import add,sub
 
 opp= input("Enter the operation:\n1.ADD\n2.SUBTRACT")
 num1 = int(input("Enter the 1st number: "))
@@ -8,9 +8,14 @@ if opp.lower()=="add":
     result= add(num1,num2)
     print('The sum is ', result)
 
+elif opp.lower()=="sub":
+    result= sub(num1,num2)
+    print('The difference is ', result)
+
 else:
     print("operation not defined. Please select addition.")
-    print('Subtraction work in progress.')    
+    print('Subtraction work in progress.')   
+   
     
 
 
