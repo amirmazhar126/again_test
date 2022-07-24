6f21773c48c6fefee6632beaea725213b85a300c
amirmazhar126 <amirmazhar126@gmail.com>
2022-07-24 12:45:07 +0530

    only add opp is executed

--- calculator.py
+++ calculator.py
@@ -1,16 +1,17 @@
 from operations import add,sub
 
-opp= input("Enter the operation:\n1.ADD\n2.SUBTRACT ")
+opp = input("Enter the operation:\n1.ADD\n2.SUBTRACT ")
 num1 = int(input("Enter the 1st number: "))
 num2 = int(input("Enter the 2nd number: "))
-num3 = int(input("Enter the 3rd number: "))
+#num3 = int(input("Enter the 3rd number: "))
 
 
 if opp.lower()=="1":
 
     result= add(num1,num2)
     print('The sum is ', result)
-
+    
+    
 else: 
     print("Operation is invalid ")    
     print("Enter the correct opp" )    
