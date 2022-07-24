8318d0aa58fb41ad83e7a60fd01bb6d1fe7be8e4
amirmazhar126 <amirmazhar126@gmail.com>
2022-07-24 12:00:24 +0530

    added new files

--- calculator.py
+++ calculator.py
@@ -1,21 +1,20 @@
 from operations import add,sub
 
-opp= input("Enter the operation:\n1.ADD\n2.SUBTRACT")
+opp= input("Enter the operation:\n1.ADD\n2.SUBTRACT ")
 num1 = int(input("Enter the 1st number: "))
 num2 = int(input("Enter the 2nd number: "))
+num3 = int(input("Enter the 3rd number: "))
+
 
 if opp.lower()=="1":
+
     result= add(num1,num2)
     print('The sum is ', result)
 
-elif opp.lower()=="2":
-    result= sub(num1,num2)
-    print('The difference is ', result)
+else: 
+    print("Operation is invalid ")    
+        
 
-else:
-    print("operation not defined. Please select addition.")
-    print('Subtraction work done.')   
+
    
     
-
-
